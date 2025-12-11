e1aa87e2a1d8145f03e97b7831c6581d74f2b86a
Satanshu Mishra <satanshumishra@outlook.com>
2025-12-10 23:38:47 -0700

    ☃️ feat(challenges): Completed Day 2, Challenge 1

--- sm/aoc25/src/main.rs
+++ sm/aoc25/src/main.rs
@@ -1,8 +1,11 @@
 mod challenge01;
 mod challenge02;
+mod challenge03;
+mod scripts;
 
 fn main() {
     println!("Hello, world!");
     // challenge01::solve();
-    challenge02::solve();
+    // challenge02::solve();
+    challenge03::solve();
 }
